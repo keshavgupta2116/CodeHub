8e0722a137cc618820d8a8790fa20fdf2c907cff
keshav gupta <keshav.gupta0021@gmail.com>
2026-04-27 23:26:45 +0530

    Removed .env and fixed database security

--- backend/database.py
+++ backend/database.py
@@ -6,7 +6,7 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://neondb_owner:npg_VveN1rMC4Skj@ep-noisy-firefly-ane7qn2v.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
